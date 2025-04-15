c7454bdd64446d36aee228cf53d49d402c67be35
harshini <harshinipamarthi@gmail.com>
2025-04-15 18:20:47 +0530

    src code

---
+++ src/test/java/com/harshini/com/AppTest.java
@@ -0,0 +1,12 @@
+package com.harshini.com;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testAppRuns() {
+        assertTrue(true, "Application should run successfully");
+    }
+}
