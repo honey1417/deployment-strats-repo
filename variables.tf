38762885a48d774195496250ea0b35c7b42a648d
harshini <harshinipamarthi@gmail.com>
2025-04-16 13:25:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The region where the resources will be created"
+  type        = string  // Change to a region with sufficient quota
+}
+
+variable "cluster_name" {
+  description = "The name of the Kubernetes cluster"
+  type        = string
+}
+
+variable "node_count" {
+  description = "The number of nodes in the Kubernetes cluster"
+  type        = number
+}
+
+variable "node_machine_type" {
+  description = "The type of machine to use for nodes in the Kubernetes cluster"
+  type        = string // Adjust based on the required machine type
+}
