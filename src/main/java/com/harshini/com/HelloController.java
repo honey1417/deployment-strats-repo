27fee0c10d6dc24e7ef384cbe4134eabd21500f1
harshini <harshinipamarthi@gmail.com>
2025-04-16 18:57:10 +0530

    Update HelloController.java

--- src/main/java/com/harshini/com/HelloController.java
+++ src/main/java/com/harshini/com/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String sayHello() {
-        return "harness-deployment-image-green";
+        return "harness-deployment-image-green-v2";
     }
 }
