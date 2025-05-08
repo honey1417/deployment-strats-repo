7f426e39a07c3e8711c01f98b8c4620ef0ec84fd
harshini <harshinipamarthi@gmail.com>
2025-05-08 14:56:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "google_container_cluster" "primary" {
   deletion_protection = false
   initial_node_count = var.node_count
   node_config {
-    machine_type = var.node_machine_type
+    machine_type = var.machine_type
     disk_size_gb = 70
   }
   remove_default_node_pool = false
